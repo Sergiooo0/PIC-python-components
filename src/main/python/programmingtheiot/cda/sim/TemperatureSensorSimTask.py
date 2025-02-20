3e380b3c972af7c9a155c514c116893c53d7bb0e
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-02-20 10:08:38 +0100

    003-completed

--- src/main/python/programmingtheiot/cda/sim/TemperatureSensorSimTask.py
+++ src/main/python/programmingtheiot/cda/sim/TemperatureSensorSimTask.py
@@ -9,8 +9,9 @@
 
 import logging
 
+import programmingtheiot.common.ConfigConst as ConfigConst
 from programmingtheiot.cda.sim.BaseSensorSimTask import BaseSensorSimTask
-from programmingtheiot.cda.sim.SensorDataGenerator import SensorDataGenerator
+from programmingtheiot.cda.sim.SensorDataGenerator import SensorDataGenerator, SensorDataSet
 
 from programmingtheiot.data.SensorData import SensorData
 
@@ -20,6 +21,11 @@ class TemperatureSensorSimTask(BaseSensorSimTask):
 	
 	"""
 
-	def __init__(self):
-		pass
+	def __init__(self, dataSet:SensorDataSet = None):
+		super(TemperatureSensorSimTask, self).__init__(
+			name = ConfigConst.TEMP_SENSOR_NAME, 
+			typeID = ConfigConst.TEMP_SENSOR_TYPE, 
+			dataSet = dataSet, 
+			minVal = SensorDataGenerator.LOW_NORMAL_INDOOR_TEMP,
+			maxVal = SensorDataGenerator.HI_NORMAL_INDOOR_TEMP)
 	
