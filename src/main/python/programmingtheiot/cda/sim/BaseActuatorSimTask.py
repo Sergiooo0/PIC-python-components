a8c808a708ce58604d9bd401e3ee2ab5d8c6f7fa
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-02-20 19:12:22 +0100

    004-completed

--- src/main/python/programmingtheiot/cda/sim/BaseActuatorSimTask.py
+++ src/main/python/programmingtheiot/cda/sim/BaseActuatorSimTask.py
@@ -21,7 +21,16 @@ class BaseActuatorSimTask():
 	"""
 
 	def __init__(self, name: str = ConfigConst.NOT_SET, typeID: int = ConfigConst.DEFAULT_ACTUATOR_TYPE, simpleName: str = "Actuator"):
-		pass
+		self.latestActuatorResponse = ActuatorData(typeID=typeID
+											 , name=name)
+		self.latestActuatorResponse.setAsResponse()
+
+		self.name = name
+		self.typeID = typeID
+		self.simpleName = simpleName
+		self.lastKnownCommand = ConfigConst.DEFAULT_COMMAND
+		self.lastKnownValue = ConfigConst.DEFAULT_VAL
+
 		
 	def getLatestActuatorResponse(self) -> ActuatorData:
 		"""
@@ -42,7 +51,41 @@ class BaseActuatorSimTask():
 		Both of these methods will have a generic implementation (logging only) within
 		this base class, although the sub-class may override if preferable.
 		"""
-		pass
+		if data and self.typeID == data.getTypeID():
+			statusCode = ConfigConst.DEFAULT_STATUS
+			curCommand = data.getCommand()
+			curVal = data.getValue()
+
+			if curCommand == self.lastKnownCommand and curVal == self.lastKnownValue:
+				logging.debug(f"Actuator command and value have not changed. Ignoring: {str(curCommand)} / {str(curVal)}")
+				return None
+			
+			logging.debug(f"Actuator command and/or value have changed. Updating: {str(curCommand)} / {str(curVal)}")
+
+			if curCommand == ConfigConst.COMMAND_ON:
+				logging.info("Activating actuator")
+				statusCode = self._activateActuator(val = data.getValue(), stateData = data.getStateData())
+			elif curCommand == ConfigConst.COMMAND_OFF:
+				logging.info("Deactivating actuator")
+				statusCode = self._deactivateActuator(val = data.getValue(), stateData = data.getStateData())
+			else:
+				logging.warning("Actuator command not recognized: %s", curCommand)
+				statusCode = -1
+
+			# update the last known command and value
+			self.lastKnownCommand = curCommand
+			self.lastKnownValue = curVal
+
+			# create the ActuatorData response from the original command
+			actuatorResponse = ActuatorData()
+			actuatorResponse.updateData(data)
+			actuatorResponse.setStatusCode(statusCode)
+			actuatorResponse.setAsResponse()
+
+			self.latestActuatorResponse.updateData(actuatorResponse)
+
+			return actuatorResponse
+		return None
 		
 	def _activateActuator(self, val: float = ConfigConst.DEFAULT_VAL, stateData: str = None) -> int:
 		"""
@@ -51,7 +94,14 @@ class BaseActuatorSimTask():
 		@param val The actuation activation value to process.
 		@param stateData The string state data to use in processing the command.
 		"""
-		pass
+		msg = "\n*******"
+		msg = msg + "\n* O N *"
+		msg = msg + "\n*******"
+		msg = msg + "\n" + self.name + " VALUE -> " + str(val) + "\n======="
+
+		logging.info(f"Simulating {self.name} actuator ON: {msg}")
+
+		return 0
 		
 	def _deactivateActuator(self, val: float = ConfigConst.DEFAULT_VAL, stateData: str = None) -> int:
 		"""
@@ -60,5 +110,13 @@ class BaseActuatorSimTask():
 		@param val The actuation activation value to process.
 		@param stateData The string state data to use in processing the command.
 		"""
-		pass
+		msg = "\n*******"
+		msg = msg + "\n* OFF *"
+		msg = msg + "\n*******"
+		msg = msg + "\n" + self.name + " VALUE -> " + str(val) + "\n======="
+
+		logging.info(f"Simulating {self.name} actuator OFF: {msg}")
+
+		return 0
+		
 		
