3f7bc413c8eeb5edb1de700ca3bcf9d5400ef060
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-02-27 19:25:52 +0100

    002 completed

--- src/main/python/programmingtheiot/cda/sim/BaseActuatorSimTask.py
+++ src/main/python/programmingtheiot/cda/sim/BaseActuatorSimTask.py
@@ -36,10 +36,10 @@ class BaseActuatorSimTask():
 		"""
 		This can return the current ActuatorData response instance or a copy.
 		"""
-		pass
+		return self.latestActuatorResponse
 	
 	def getSimpleName(self) -> str:
-		pass
+		return self.simpleName
 	
 	def updateActuator(self, data: ActuatorData) -> bool:
 		"""
